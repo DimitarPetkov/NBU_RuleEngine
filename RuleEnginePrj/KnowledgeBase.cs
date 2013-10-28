24c6b7a96ea1fbf37cc46d0f8ce6ecfe3da30d2c
DimitarPetkov <d.petkoff@abv.bg>
2013-10-28 23:02:25 +0200

    common algorithm

--- RuleEnginePrj/KnowledgeBase.cs
+++ RuleEnginePrj/KnowledgeBase.cs
@@ -10,11 +10,38 @@ namespace Model
         private Rule[] agenda;
         private Rule[] ruleLib;
         private Fact[] facts;
-
+        private String targetVariable;
 
         private void init()
         {
+            //init rules
 
+            //init facts
         }
+
+        public void setTargetVar(string target)
+        {
+            this.targetVariable = target;
+        }
+
+        public void evalTarget()
+        {
+            //fire & assert facts from rules where possible - forward chaining
+            //dismiss fired rules
+
+            //is target in facts
+
+            //while target not found
+                //find rule with least unknown conditions
+                // try to eval condition variables of selected rule - could be recursive
+                //if not enough - ask user about fact
+                    //do
+                        //assert fact 
+                        // do forward chaining
+                    //while asserting new facts 
+                
+        }
+
+
     }
 }
