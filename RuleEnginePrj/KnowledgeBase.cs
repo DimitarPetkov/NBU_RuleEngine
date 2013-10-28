552ccb3eb5f9148339067e1dc6a3fd822e346b11
DimitarPetkov <d.petkoff@abv.bg>
2013-10-28 21:29:48 +0200

    initial

---
+++ RuleEnginePrj/KnowledgeBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Model
+{
+    class KnowledgeBase
+    {
+        private void init()
+        {
+
+        }
+    }
+}
---
+++ RuleEnginePrj/Rule.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Model
+{
+    class Rule
+    {
+        private bool isActivated;
+        private bool isFired;
+
+        String[] conditionVariables;
+        String[] actionVariables;
+        String condition;
+        String action;
+        
+        public void fire()
+        {
+            
+        }
+    }
+}
